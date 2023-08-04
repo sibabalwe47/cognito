473855dbe2748740398d6598de71810fe65352e7
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-08-04 07:32:24 +0200

    Added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "userpool_arn" {
+  value = aws_cognito_user_pool.this.arn
+}
 
+output "userpool_id" {
+  value = aws_cognito_user_pool.this.id
+}
+
+output "userpool_client_id" {
+  value = aws_cognito_user_pool_client.this.id
+}
